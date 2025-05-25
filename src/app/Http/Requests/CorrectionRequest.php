b2045b82fb60668243cf2b60f3aa93cd9e3851ed
Doragon-RH <r216af@icloud.com>
2025-05-26 08:15:14 +0900

    バリデーションルール、エラrーメッセージの変更

--- src/app/Http/Requests/CorrectionRequest.php
+++ src/app/Http/Requests/CorrectionRequest.php
@@ -22,37 +22,40 @@ class CorrectionRequest extends FormRequest
      * @return array
      */
     public function rules()
-{
-    return [
-        'new_clock_in'   => 'required|date_format:H:i',
-        'new_clock_out'  => 'required|date_format:H:i|after:new_clock_in',
+    {
+        return [
+            'new_clock_in'  => 'required|date_format:H:i',
+            'new_clock_out' => 'required|date_format:H:i|after:new_clock_in',
 
-        // ★ 配列で受け取る宣言
-        'new_break_in'   => 'nullable|array',
-        'new_break_out'  => 'nullable|array',
+            'new_break_in'   => 'nullable|array',
+            'new_break_out'  => 'nullable|array',
 
-        // ★ 配列中の各要素をチェック
-        'new_break_in.*'  => 'nullable|date_format:H:i|after:new_clock_in|before:new_break_out.*',
-        'new_break_out.*' => 'nullable|date_format:H:i|after:new_break_in.*|before:new_clock_out',
+            // 各要素が出勤後・退勤前であることをチェック
+            'new_break_in.*'  => 'nullable|date_format:H:i|after:new_clock_in|before:new_clock_out',
+            'new_break_out.*' => 'nullable|date_format:H:i|after:new_clock_in|before:new_clock_out',
 
-        'comment'        => 'required',
-    ];
-}
-
+            'comment'       => 'required',
+        ];
+    }
 
     public function messages()
     {
         return [
-            'new_clock_in.required' => '出勤時間を入力してください。',
-            'new_clock_out.required' => '退勤時間を入力してください。',
-            'new_clock_out.after' => '出勤時間もしくは退勤時間が不適切な値です。',
+            'new_clock_in.required'    => '出勤時間を入力してください。',
+            'new_clock_in.date_format' => '出勤時間は「HH:mm」形式で入力してください。',
+            'new_clock_out.required'   => '退勤時間を入力してください。',
+            'new_clock_out.date_format'=> '退勤時間は「HH:mm」形式で入力してください。',
+            'new_clock_out.after'      => '出勤時間もしくは退勤時間が不適切な値です。',
+
             'new_break_in.*.date_format'  => '休憩開始は「HH:mm」形式で入力してください。',
-            'new_break_in.*.after'        => '休憩開始は出勤時間以降の時刻を選択してください。',
-            'new_break_in.*.before'       => '休憩開始は休憩終了より前の時刻を選択してください。',
+            'new_break_in.*.before'       => '休憩時間が勤務時間外です。',
+            'new_break_in.*.after'        => '休憩時間が勤務時間外です。',
+
             'new_break_out.*.date_format' => '休憩終了は「HH:mm」形式で入力してください。',
-            'new_break_out.*.after'       => '休憩終了は休憩開始以降の時刻を選択してください。',
-            'new_break_out.*.before'      => '休憩終了は退勤時間より前の時刻を選択してください。',
-            'comment.required' => '備考を記入してください。'
+            'new_break_out.*.before'      => '休憩時間が勤務時間外です。',
+            'new_break_out.*.after'       => '休憩時間が勤務時間外です。',
+
+            'comment.required' => '備考を記入してください。',
         ];
     }
 }
